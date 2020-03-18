86349e17e630e5eff486ff46cf23afc71db845b7
Taz Uddin <getleton@gmail.com>
2020-03-19 00:13:14 +0600

    Simplest net core web app

--- NetCore.WebApi/Common/Dispatchers/CommandDispatcher.cs
+++ NetCore.WebApi/Common/Dispatchers/CommandDispatcher.cs
@@ -12,6 +12,12 @@ namespace NetCore.WebApi.Common.Dispatchers
     public class CommandDispatcher : ICommandDispatcher
     {
         private readonly IComponentContext _context;
+
+        public CommandDispatcher(IComponentContext context)
+        {
+            _context = context;
+        }
+
         public Task SendAsync<TCommand>(TCommand command) where TCommand : ICommand
         {
             return _context.Resolve<ICommandHandler<TCommand>>().HandleAsync(command, CorrelationContext.Emtpy);
---
+++ NetCore.WebApi/Common/RabbitMq/BusPublisher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetCore.WebApi.Common.Messages;
+using RabbitMQ;
+namespace NetCore.WebApi.Common.RabbitMq
+{
+    public class BusPublisher : IBusPublisher
+    {
+        
+        
+        public BusPublisher()
+        {
+            
+        }
+
+        public Task PublishAsync<TEvent>(TEvent @event, ICorrelationContext context) where TEvent : IEvent
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendAsync<TCommand>(TCommand command, ICorrelationContext context) where TCommand : ICommand
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
